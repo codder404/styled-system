47b0aab3af4ddfe6ddfbfaa7903c13644268f742
Fernando dos Santos <nando4803@gmail.com>
2020-10-17 15:13:41 +0100

    :rocket: add colors styleguide

---
+++ src/Styleguide/colors.ts
@@ -0,0 +1,23 @@
+export interface ThemeColors {
+  primary: string;
+  link: string;
+  success: string;
+  warning: string;
+  error: string;
+  heading: string;
+  text: string;
+  disabled: string;
+  border: string;
+}
+
+export const colors: ThemeColors = {
+  primary: '#423EA2',
+  link: '#1890ff',
+  success: '#52c41a',
+  warning: '#faad14',
+  error: '#e84118',
+  heading: '#423EA2',
+  text: '#000',
+  disabled: '#f5222d',
+  border: '#423EA2',
+}
