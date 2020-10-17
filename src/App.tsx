b7da01a90faa99c1e9b908fb8607e7fc4528798e
Fernando dos Santos <nando4803@gmail.com>
2020-10-17 12:37:41 +0100

    :rocket: add text to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import { Text } from './components';
 import GlobalStyle from './styles/styles';
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <Text />
     </>
   );
 }
